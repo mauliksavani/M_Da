4f360628aea00e7fe21e16be03eb00ac878fd14f
dhruv <dhruv@gmail.com>
2024-04-24 15:30:46 +0530

    ABCD

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-my name is dhruv;
-my friend name is maulik
-my friend maulik jiweciwncinwicniwnciwnixuniwnjiwncibwicbwicniwnicknudhuchwuhcuwhcuwhuchwucuwhcuwcuwhcuwcuwuchuwhcuwucuwcuwuc
-
-
-
-hello
-dhruv bro
-=======
-my friend name is dhruv
-in father name is navnit bhai
->>>>>>> 5f4f38dee5b85ca574f7c67e62690c776f25bb13
+ABCD
